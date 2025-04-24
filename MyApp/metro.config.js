20e4a7c33d71ac57e0e6d4fe5487aed2fac852e2
Mehar Saini <msa469@sfu.ca>
2025-04-23 17:38:45 -0700

    Working Tamagui

--- MyApp/metro.config.js
+++ MyApp/metro.config.js
@@ -1,11 +1,10 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+// Learn more https://docs.expo.io/guides/customizing-metro
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 
-/**
- * Metro configuration
- * https://reactnative.dev/docs/metro
- *
- * @type {import('@react-native/metro-config').MetroConfig}
- */
-const config = {};
+const config = {
+  resolver: {
+    sourceExts: ['js', 'jsx', 'json', 'ts', 'tsx', 'mjs'],
+  },
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
