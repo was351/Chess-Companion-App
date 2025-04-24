6dded1057fa0934e78025a38b01ffa9f1aefc5bf
Wasif <wasif_farhan@hotmail.com>
2025-04-23 22:26:00 -0700

    fixed version mismatch

--- MyApp/App.tsx
+++ MyApp/App.tsx
@@ -1,9 +1,17 @@
 import { createTamagui } from '@tamagui/core'
 import { TamaguiProvider } from 'tamagui'
 import { defaultConfig } from '@tamagui/config/v4'
-import { Button } from 'tamagui'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
+import 'react-native-gesture-handler'
 
-// you usually export this from a tamagui.config.ts file
+// Import your screens
+import HomeScreen from './screens/HomeScreen.tsx'
+import AboutScreen from './screens/AboutScreen.tsx'
+import SettingsScreen from './screens/SettingsScreen.tsx'
+
+// Create Tamagui config
 const config = createTamagui(defaultConfig)
 
 type Conf = typeof config
@@ -13,10 +21,21 @@ declare module '@tamagui/core' {
   interface TamaguiCustomConfig extends Conf {}
 }
 
-export default function Demo() {
+// Create the stack navigator
+const Stack = createNativeStackNavigator()
+
+export default function App() {
   return (
-    <TamaguiProvider config={config}>
-      <Button theme="blue">Hello world</Button>
-    </TamaguiProvider>
+    <SafeAreaProvider>
+      <TamaguiProvider config={config}>
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName="Home">
+            <Stack.Screen name="Home" component={HomeScreen} />
+            <Stack.Screen name="About" component={AboutScreen} />
+            <Stack.Screen name="Settings" component={SettingsScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </TamaguiProvider>
+    </SafeAreaProvider>
   )
 }
