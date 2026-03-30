81d48a140eaa0e6b246fe65fb0a33bf4cc39645a
Wasif <wasif_farhan@hotmail.com>
2026-03-29 18:27:38 -0700

    adds testing framework plus GI actions

---
+++ Board-Backend/tests/framework/__init__.py
@@ -0,0 +1,5 @@
+"""Test helpers that model multi-device / multi-player API usage."""
+
+from tests.framework.online_session import OnlineGameSession, set_active_player
+
+__all__ = ["OnlineGameSession", "set_active_player"]
